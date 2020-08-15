cd723b5ee465e48dc9abb844cff4ead495032fb3
Touhidul Shawan <touhidulshawan@gmail.com>
2020-08-15 09:56:51 +0600

    fix style

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,23 +19,23 @@ const Modal = (props) => {
             </div>
             {props.children}
             {props.productCount === 0 ? (
-              <h2 className="text-center text-2xl text-gray-500 tracking-wider">
+              <h2 className="text-center text-2xl text-gray-700 tracking-wider">
                 There is no Product in your Cart
               </h2>
             ) : null}
           </div>
-          <div className="flex md:flex-row  md:justify-between md:items-center flex-col items-start bg-purple-600 p-5 mt-auto ">
+          <div className="flex md:flex-row  md:justify-between md:items-center flex-col items-start bg-gray-200  p-5 mt-auto ">
             <div className="flex justify-center items-center">
-              <span className=" text-purple-200  tracking-wide text-xl font-bold mr-2">
+              <span className="tracking-wide text-gray-600 text-xl  mr-2">
                 Total Price:{" "}
               </span>
-              <p className="text-xl bg-purple-900 tracking-wider py-1 px-3 rounded shadow text-purple-200 font-bold">
+              <p className="text-xl bg-pink-200  tracking-wider py-1 px-3 text-pink-700 rounded">
                 ${props.price}
               </p>
             </div>
             <button
               disabled={props.price === 0}
-              className="btn_checkout hover:bg-purple-800 hover:text-purple-300 focus:outline-none focus:shadow-outline m-auto mt-6 md:m-0 md:mt-0"
+              className="btn_checkout focus:outline-none focus:shadow-outline  m-auto mt-6 md:m-0 md:mt-0"
             >
               Checkout
             </button>
