c1d5779779549467f35a02996f144eca59a0c9da
Touhidul Shawan <touhidulshawan@gmail.com>
2020-08-15 09:12:45 +0600

    Updates

---
+++ precache-manifest.69e82a89608d4bc60d90f81eb12051c7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c1457a0febd0fbfb67043c62a77306",
+    "url": "/shopping_cart/index.html"
+  },
+  {
+    "revision": "95c79accb4b261e4da6b",
+    "url": "/shopping_cart/static/css/main.5d73541b.chunk.css"
+  },
+  {
+    "revision": "80e40abd9274da026b01",
+    "url": "/shopping_cart/static/js/2.f25e88eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping_cart/static/js/2.f25e88eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95c79accb4b261e4da6b",
+    "url": "/shopping_cart/static/js/main.ed572b12.chunk.js"
+  },
+  {
+    "revision": "b35a2a719d7c6ad482b5",
+    "url": "/shopping_cart/static/js/runtime-main.de0df27a.js"
+  },
+  {
+    "revision": "680515396ab2a9a23a0b86d17aff45e9",
+    "url": "/shopping_cart/static/media/apple.68051539.jpg"
+  },
+  {
+    "revision": "2f048185b019776b3e11d657e5161a36",
+    "url": "/shopping_cart/static/media/avocado.2f048185.jpg"
+  },
+  {
+    "revision": "b35422c429b6c20bd1a16b7e403bcda0",
+    "url": "/shopping_cart/static/media/coconut.b35422c4.jpg"
+  },
+  {
+    "revision": "3464a8830b7323230710dc92d52afd7a",
+    "url": "/shopping_cart/static/media/grapes.3464a883.jpg"
+  },
+  {
+    "revision": "486644dd950b725f33d230dfc5129ce5",
+    "url": "/shopping_cart/static/media/orange.486644dd.jpg"
+  },
+  {
+    "revision": "6eb7b293d1c577836cbcb656b0db22cd",
+    "url": "/shopping_cart/static/media/pineapple.6eb7b293.jpg"
+  },
+  {
+    "revision": "d9e5c8479903a558fb4bfd362ccd03e1",
+    "url": "/shopping_cart/static/media/strawberry.d9e5c847.jpg"
+  }
+]);
